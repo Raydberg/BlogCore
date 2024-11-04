4d83a66ee20c5296f8951c5d64ce501926cf1eac
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-11-04 15:23:14 -0500

    Añadir entidad Categoria y repositorio genérico
    - Añadida la clase `Categoria` en `Categoria.cs` con propiedades `Id`, `Nombre` y `Orden`.
    - Modificado `ApplicationDbContext.cs` para incluir `DbSet<Categoria>`.
    - Actualizado `ApplicationDbContextModelSnapshot.cs` para reflejar la nueva entidad `Categoria`.
    - Creada migración `20241104151243_migration for categories` para crear la tabla `Categorias`.
    - Añadida la interfaz `IRepository<T>` en `IRepository.cs` para operaciones CRUD genéricas.
    - Implementada la clase `Repository<T>` en `Repository.cs` utilizando Entity Framework Core.

---
+++ BlogCore.AccesoDatos/Data/Repository/Repository.cs
@@ -0,0 +1,70 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BlogCore.AccesoDatos.Data.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly DbContext Context;
+    internal DbSet<T> dbSet;
+
+    public Repository (DbContext context)
+    {
+        Context = context;
+        dbSet = context.Set<T>();
+    }
+
+
+    public void Add (T entity) { dbSet.Add(entity); }
+
+    public T Get (int id) { return dbSet.Find(id); }
+
+
+    public IEnumerable<T> GetAll (Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null
+        , string includeProperties = null)
+    {
+        //Se crea una consulta IQueryable a partir del dbSet del conetxto
+        IQueryable<T> query = dbSet;
+        //Se aplica filtro si se proporciona
+        if (filter != null) query = query.Where(filter);
+        //Se incluye propiedades de navegacion si se proporcionan
+        if (includeProperties != null)
+            //Se divide la cadena de propiedades por coma y se itera sobre ellas
+            foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                query = query.Include(includeProperty);
+        //Se aplica el ordenamiento se si proporciona
+        if (orderBy != null) return orderBy(query).ToList();
+        return query.ToList();
+    }
+
+    public T GetFirstOrDefault (Expression<Func<T, bool>>? filter = null, string includeProperties = null)
+    {
+        //Se crea una consulta IQueryable a partir del dbSet del conetxto
+        IQueryable<T> query = dbSet;
+        //Se aplica filtro si se proporciona
+        if (filter != null) query = query.Where(filter);
+        // Se incluye propiedades de navegacion si se proporcionan
+        if (includeProperties != null)
+            //Se divide la cadena de propiedades por coma y se itera sobre ellas
+            foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                query = query.Include(includeProperty);
+
+        return query.FirstOrDefault();
+    }
+
+    public void Remove (int id)
+    {
+        T entityToRemove = dbSet.Find(id);
+        if (entityToRemove != null)
+        {
+            dbSet.Remove(entityToRemove);
+        }
+
+    }
+
+    public void Remove (T entity)
+    {
+        dbSet.Remove(entity);
+    }
+}
---
+++ BlogCore.Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogCore.Models;
+
+public class Categoria
+{
+    [Key] public int Id { get; set; }
+
+    [Required(ErrorMessage = "Ingrese un nombre para la categoria")]
+    [Display(Name = "Nombre de Categoria")]
+    public string Nombre { get; set; }
+    [Display(Name = "Orden de visualizacion")]
+    public int? Orden { get; set; }
+
+
+}
