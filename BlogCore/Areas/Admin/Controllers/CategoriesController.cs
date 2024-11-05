4f7b1d20b8063b082ae3d8e730e860b7280f7773
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-11-04 20:11:19 -0500

    Actualización de dependencias y nuevas funcionalidades

---
+++ BlogCore/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly IContainerWork _containerWork;
+
+        public CategoriesController (IContainerWork containerWork)
+        {
+            _containerWork = containerWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        #region Llamadas a la API
+        [HttpGet]
+        public IActionResult GetAll ()
+        {
+            return Json(new { data = _containerWork.Category.GetAll() });
+        }
+
+
+        #endregion
+    }
+}
